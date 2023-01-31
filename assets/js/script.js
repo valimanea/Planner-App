ca8b60e4d1f77e7d11ca5b93ac4f0b7d067daddb
Vali Manea <118940094+valimanea@users.noreply.github.com>
2023-01-31 08:08:50 +0000

    update project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,75 @@
-var timeDisplayEl = $('#currentDay');
-var timeBlocksEl = $('#calendar');
+var timeDisplayEl = $("#currentDay");
+var timeBlocksEl = $("#calendar");
 
-var timeIndex = ["9AM","10AM","11AM", "12AM","1PM","2PM","3PM","4PM","5PM"];
-
+var timeIndex = [
+  "9AM",
+  "10AM",
+  "11AM",
+  "12AM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+];
 
 // handle displaying the time
 function displayTime() {
-    var rightNow = moment().format('dddd, MMMM Do');
-    timeDisplayEl.text(rightNow);
-  }
-
-var timeCompare = moment().hour();
-
-for (var i = 0; i < timeIndex.length; i++) {
-    var listEl = $('<div>').addClass("row time-block");
-    var timeEl = $('<div>').addClass("hour col-lg-2 col-md-2 col-sm-12")
-        .text(timeIndex[i]);
-    var textEl = $('<textarea>').addClass("description col-lg-9 col-md-9 col-sm-12")
-        .attr("id",timeIndex[i]);
-
-    if(timeCompare<[i+9]){
-        textEl.addClass("future");
-    }
-    else if(timeCompare>[i+9]){
-        textEl.addClass("past");
-    }
-    else{
-        text.addClass("present");
-    };
-
-    var saveEl = $('<button>')
-        .addClass("saveBtn col-lg-1 col-md-1 col-sm-12")
-        .attr("data-letter", timeIndex[i])
-        .text("Save");
-    listEl.append(timeEl);
-    listEl.append(textEl);
-    listEl.append(saveEl);
-    timeBlocksEl.append(listEl);
+  var rightNow = moment().format("dddd, MMMM Do");
+  timeDisplayEl.text(rightNow);
 }
 
-$(".saveBtn").on("click", function() {
-    
-    var textId = "#" + $(this).attr("data-letter");
-    var text = $(textId).val();
-    console.log(text);
+var timeCompare = moment().hour();
+var storedText = [];
 
-    // var test2 = $(event.target);
-    // console.log(test2);
+function readSavedText() {
+  for (var i = 0; i < timeIndex.length; i++) {
+    if (JSON.parse(localStorage.getItem(timeIndex[i])) == null) {
+      storedText[i] = "";
+    } else {
+      storedText[i] = JSON.parse(localStorage.getItem(timeIndex[i]));
+    }
+  }
+}
 
-    // var test3 = test2.parent('div');
-    // console.log(test3);
+readSavedText();
 
-    // var test4 = $(test5).val();
-    // console.log(test4);
-  });
+for (var i = 0; i < timeIndex.length; i++) {
+  var listEl = $("<div>").addClass("row time-block");
+  var timeEl = $("<div>")
+    .addClass("hour col-lg-2 col-md-2 col-sm-3")
+    .text(timeIndex[i]);
+  var textEl = $("<textarea>")
+    .addClass("description col-lg-9 col-md-9 col-sm-7")
+    .attr("id", timeIndex[i])
+    .text(storedText[i]);
 
+  if (timeCompare < [i + 9]) {
+    textEl.addClass("future");
+  } else if (timeCompare > [i + 9]) {
+    textEl.addClass("past");
+  } else {
+    text.addClass("present");
+  }
+
+  var saveEl = $("<button>")
+    .addClass("saveBtn col-lg-1 col-md-1 col-sm-2")
+    .attr("data-letter", timeIndex[i])
+    .text("Save");
+
+  listEl.append(timeEl);
+  listEl.append(textEl);
+  listEl.append(saveEl);
+  timeBlocksEl.append(listEl);
+}
+
+$(".saveBtn").on("click", function () {
+  var Id = $(this).attr("data-letter");
+  var textId = "#" + Id;
+  var text = $(textId).val();
+  //   console.log(text);
+
+  localStorage.setItem(Id, JSON.stringify(text));
+});
 
 setInterval(displayTime, 1000);
-
-
-
-
